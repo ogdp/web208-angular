b3ab7026dec314cf69c91a61d6de475119324063
MinhDuc <ogdp.vn@gmail.com>
2023-05-13 00:52:13 +0700

    Cập nhật header footer home chứa sản phẩm

---
+++ Step.md
@@ -0,0 +1,6 @@
+## install boostrap + boostrap-icon
+
+npm
+custome file angular.json style : "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+and import from styles.css
+import "./node_modules/bootstrap-icons/font/bootstrap-icons.min.css"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { ListProductComponent } from './pages/product/list-product/list-product.component';
+import { DetailsProductComponent } from './pages/product/details-product/details-product.component';
+import { CardComponent } from './components/product/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AuthComponent,
+    ListProductComponent,
+    DetailsProductComponent,
+    CardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
