891cab3cb10ad74c5c52a269d30c3d3897874cac
MinhDuc <ogdp.vn@gmail.com>
2023-05-19 15:18:40 +0700

    Merge pages Admin & Client folder + Theme Dashboard layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,39 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { ListProductComponent } from './pages/product/list-product/list-product.component';
-import { DetailsProductComponent } from './pages/product/details-product/details-product.component';
-import { CardComponent } from './components/product/card/card.component';
-import { BannerComponent } from './components/banner/banner.component';
+
+// Client
+import { HeaderComponent } from './components/client/header/header.component';
+import { FooterComponent } from './components/client/footer/footer.component';
+import { HomeComponent } from './pages/client/home/home.component';
+import { AuthComponent } from './pages/client/auth/auth.component';
+import { ListProductComponent } from './pages/client/product/list-product/list-product.component';
+import { DetailsProductComponent } from './pages/client/product/details-product/details-product.component';
+import { CardComponent } from './components/client/card/card.component';
+import { BannerComponent } from './components/client/banner/banner.component';
 import { SwiperModule } from 'swiper/angular';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ProductsNewComponent } from './components/products-new/products-new.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CollectionsComponent } from './components/collections/collections.component';
-import { ProductsFamousComponent } from './components/products-famous/products-famous.component';
-import { AccountComponent } from './pages/account/account.component';
-import { ContactAboutUsComponent } from './components/contact-about-us/contact-about-us.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ProductsNewComponent } from './components/client/products-new/products-new.component';
+import { CartComponent } from './components/client/cart/cart.component';
+import { CollectionsComponent } from './components/client/collections/collections.component';
+import { ProductsFamousComponent } from './components/client/products-famous/products-famous.component';
+import { AccountComponent } from './pages/client/account/account.component';
+import { ContactAboutUsComponent } from './components/client/contact-about-us/contact-about-us.component';
 import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CheckoutSuccessComponent } from './pages/checkout-success/checkout-success.component';
+import { CheckoutSuccessComponent } from './pages/client/checkout-success/checkout-success.component';
+// Blank
 import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+// Admin
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { NavAdminComponent } from './components/admin/nav-admin/nav-admin.component';
+import { HeaderAdminComponent } from './components/admin/header-admin/header-admin.component';
+import { CheckoutAdminComponent } from './pages/admin/checkout-admin/checkout-admin.component';
+import { ProductAdminComponent } from './pages/admin/product-admin/product-admin.component';
+import { UserAdminComponent } from './pages/admin/user-admin/user-admin.component';
+import { FeedbackAdminComponent } from './pages/admin/feedback-admin/feedback-admin.component';
+import { BannerAdminComponent } from './pages/admin/banner-admin/banner-admin.component';
+import { SettingAdminComponent } from './pages/admin/setting-admin/setting-admin.component';
 
 @NgModule({
   declarations: [
@@ -49,6 +61,14 @@ import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.compon
     DashboardComponent,
     CheckoutSuccessComponent,
     BlankLayoutComponent,
+    NavAdminComponent,
+    HeaderAdminComponent,
+    CheckoutAdminComponent,
+    ProductAdminComponent,
+    UserAdminComponent,
+    FeedbackAdminComponent,
+    BannerAdminComponent,
+    SettingAdminComponent,
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/pages/admin/checkout-admin/checkout-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-admin',
+  templateUrl: './checkout-admin.component.html',
+  styleUrls: ['./checkout-admin.component.css']
+})
+export class CheckoutAdminComponent {
+
+}
