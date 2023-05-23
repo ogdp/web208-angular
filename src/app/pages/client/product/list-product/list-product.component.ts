b69508d87245f4ad66135750b53f477a66ce995b
MinhDuc <ogdp.vn@gmail.com>
2023-05-23 23:56:16 +0700

    bug 2ng-search-fillter

--- src/app/pages/client/product/list-product/list-product.component.ts
+++ src/app/pages/client/product/list-product/list-product.component.ts
@@ -1,23 +1,19 @@
 import { Component } from '@angular/core';
 import { products } from 'src/app/data/mockData';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { IProduct } from 'src/common/Product';
-
 
 @Component({
   selector: 'app-list-product',
   templateUrl: './list-product.component.html',
   styleUrls: ['./list-product.component.css'],
 })
-
 export class ListProductComponent {
-  products:IProduct[] = products;
-  searchTerm!: string
+  products: IProduct[] = products;
+  searchTerm!: string;
   filteredProducts: IProduct[] = [];
 
-
   filterProducts() {
-    return this.products.filter(product => {
+    return this.products.filter((product) => {
       return product.name.includes(this.searchTerm);
     });
   }
